6cd497aae3d02258e18b18ee1147dafbc6a2ff26
Larry Mateo Ramirez Castro <larry.ramirez@correounivalle.edu.co>
2022-07-23 18:44:45 -0500

    Finish the point 2.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 const main = () => {
-    const color = querySelector('.theme-switch input[type=checkbox]');
+    
+    const colorSwitch = document.getElementById('checkbox');
 
     function switchColor(e){
-        if
+        if(e.target.checked){
+            document.documentElement.setAttribute('data-theme', 'theme2');
+        }else {
+            document.documentElement.setAttribute('data-theme', 'theme1');
+        }
     }
+
+    colorSwitch.addEventListener('change', switchColor, false);
 
 }
